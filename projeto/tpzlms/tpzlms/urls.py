03570ab4a65f84fc8618eb565015dd71aad33974
Notim_ <paulino.joaovitor@yahoo.com.br>
2018-10-10 20:27:29 -0300

    Up

---
+++ projeto/tpzlms/tpzlms/urls.py
@@ -0,0 +1,17 @@
+from cadastroaluno import views
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+
+admin.autodiscover()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^cadastro/$', views.Criar.as_view(), name='cadastro'),
+    url(r'^lista/$', views.Lista.as_view(), name='lista'),
+    #url(r'^admin/', include(admin.site.urls)),
+]   
